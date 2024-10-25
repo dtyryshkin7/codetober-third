a8128d99f4779203e550b4cb86bf761322ba2599
dtyryshkin7 <dtyryshkin@hotmail.com>
2024-10-25 11:07:23 +0000

    fix bug

--- files/modules/vnet/main.tf
+++ files/modules/vnet/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_virtual_network" "vnet" {
 
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
-  resource_group_name  = azurerm_virtual_network.vnet.location
+  resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.subnet_prefixes
 }
